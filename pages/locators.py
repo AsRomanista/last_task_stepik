43ef62d8914c1a1bcd3a06d0316fe05155de4eeb
AsRomanista <dmitry.garkovitch@gmail.com>
2023-07-28 00:35:19 +0200

    alert, check login_page.py

--- pages/locators.py
+++ pages/locators.py
@@ -3,3 +3,9 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+class LoginPageLocators():
+    LOGIN_BUTTON = (By.CSS_SELECTOR, "button[value='Log In']")
+    REGISTER_BUTTON = (By.CSS_SELECTOR, "button[value='Register']")
+
+
---
+++ pages/login_page.py
@@ -0,0 +1,22 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # реализуйте проверку на корректный url адрес
+        url = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+        assert "login" in self.url, "This isn't url page"
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.LOGIN_BUTTON), "This isn't login_button"
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.REGISTER_BUTTON), "This isn't register_button"
