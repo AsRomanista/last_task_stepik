5e63fe8e36dd0d9db44e557cd358df2d2b2555e0
AsRomanista <dmitry.garkovitch@gmail.com>
2023-07-30 00:58:04 +0200

    Задание: наследование и отрицательные проверки

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,9 +1,11 @@
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from .locators import BasePageLocators
 
 
 class BasePage():
+
     def __init__(self, browser, url):
         self.browser = browser
         self.url = url
@@ -15,6 +17,13 @@ class BasePage():
         self.browser = browser
         self.url = url
         self.browser.implicitly_wait(timeout)
+
+    def go_to_login_page(self):
+        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK_INVALID)
+        link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
 
     def is_element_present(self, how, what):
         try:
@@ -41,5 +50,9 @@ class BasePage():
             return False
 
         return True
+
+    def go_to_basket_page(self):
+        link = self.browser.find_element(*BasePageLocators.BASKET_LINK)
+        link.click()
 
 
--- pages/locators.py
+++ pages/locators.py
@@ -1,8 +1,5 @@
 from selenium.webdriver.common.by import By
 
-class MainPageLocators:
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
-
 class LoginPageLocators():
     LOGIN_BUTTON = (By.CSS_SELECTOR, "button[value='Log In']")
     REGISTER_BUTTON = (By.CSS_SELECTOR, "button[value='Register']")
@@ -11,5 +8,16 @@ class LoginPageLocators():
     BASKET_TOTAL_MESSAGE = (By.CSS_SELECTOR, ".alert-info .alertinner strong")
     PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main h1")
     PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
+
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    BASKET_LINK = (By.CSS_SELECTOR, ".basket-mini a")
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
+
+class BasketPageLocators():
+    BASKET_ITEMS = (By.CSS_SELECTOR, ".basket-items")
+    EMPTY_BASKET_MESSAGE = (By.CSS_SELECTOR, "#content_inner > p")
+
 
 
